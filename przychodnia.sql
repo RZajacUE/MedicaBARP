5cc1053016086d13b0af6003603c8d295b659132
balix1212 <150184954+balix1212@users.noreply.github.com>
2023-11-19 18:10:07 +0100

    Update przychodnia.sql

--- przychodnia.sql
+++ przychodnia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Lis 16, 2023 at 11:22 AM
+-- Generation Time: Lis 19, 2023 at 05:59 PM
 -- Wersja serwera: 10.4.28-MariaDB
 -- Wersja PHP: 8.2.4
 
@@ -29,31 +29,31 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `pacjenci` (
   `IdPacjenta` int(11) NOT NULL,
-  `Imie` varchar(255) DEFAULT NULL,
-  `Nazwisko` varchar(255) DEFAULT NULL,
-  `Pesel` varchar(11) DEFAULT NULL,
-  `DataUrodzenia` date DEFAULT NULL,
-  `Adres` varchar(255) DEFAULT NULL,
+  `Imie` varchar(50) NOT NULL,
+  `Nazwisko` varchar(50) NOT NULL,
+  `Pesel` varchar(11) NOT NULL,
+  `DataUrodzenia` date NOT NULL,
   `NumerTel` varchar(20) DEFAULT NULL,
-  `Haslo` varchar(255) DEFAULT NULL,
-  `Email` varchar(255) DEFAULT NULL
+  `Haslo` varchar(255) NOT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `Plec` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
 
 --
 -- Dumping data for table `pacjenci`
 --
 
-INSERT INTO `pacjenci` (`IdPacjenta`, `Imie`, `Nazwisko`, `Pesel`, `DataUrodzenia`, `Adres`, `NumerTel`, `Haslo`, `Email`) VALUES
-(1, 'Jan', 'Kowalski', '12345678901', '1980-05-15', 'ul. Kwiatowa 7', '123456789', 'g87!eDja', 'UeKat1@gmail.com'),
-(2, 'Anna', 'Nowak', '98765432109', '1995-08-20', 'ul. Słoneczna 14', '987654321', 'dA!123rt', 'UeKat2@gmail.com'),
-(3, 'Marek', 'Wiśniewski', '56789012345', '1976-12-10', 'ul. Leśna 3', '567890123', 'S34!ikds', 'UeKat3@gmail.com'),
-(4, 'Ewa', 'Jankowska', '34567890123', '1990-03-25', 'ul. Parkowa 11', '345678901', 'Gg!g54i4', 'UeKat4@gmail.com'),
-(5, 'Piotr', 'Kowalczyk', '78901234567', '1985-09-08', 'ul. Polna 9', '789012345', 'Lio8!tya', 'UeKat5@gmail.com'),
-(6, 'Alicja', 'Lis', '45678901234', '2000-07-30', 'ul. Wiosenna 2', '456789012', 'UeKat66!', 'UeKat6@gmail.com'),
-(7, 'Kamil', 'Kaczor', '23456789012', '1972-01-14', 'ul. Główna 4', '234567890', 'UeKat77!', 'UeKat7@gmail.com'),
-(8, 'Karolina', 'Wójcik', '65432109876', '1988-11-05', 'ul. Cicha 8', '654321098', 'UeKat88!', 'UeKat8@gmail.com'),
-(9, 'Wojciech', 'Nowicki', '87654321098', '1993-04-18', 'ul. Zielona 12', '876543210', 'UeKat99!', 'UeKat9@gmail.com'),
-(10, 'Magdalena', 'Duda', '11223344556', '1974-06-22', 'ul. Różana 6', '112233445', 'UeKat00!', 'UeKat10@gmail.com');
+INSERT INTO `pacjenci` (`IdPacjenta`, `Imie`, `Nazwisko`, `Pesel`, `DataUrodzenia`, `NumerTel`, `Haslo`, `Email`, `Plec`) VALUES
+(1, 'Jan', 'Kowalski', '12345678901', '1980-05-15', '123456789', 'UeKat11!', 'UeKat1@gmail.com', 'Mężczyzna'),
+(2, 'Anna', 'Nowak', '98765432109', '1995-08-20', '987654321', 'UeKat22!', 'UeKat2@gmail.com', 'Kobieta'),
+(3, 'Marek', 'Wiśniewski', '56789012345', '1976-12-10', '567890123', 'UeKat33!', 'UeKat3@gmail.com', 'Mężczyzna'),
+(4, 'Ewa', 'Jankowska', '34567890123', '1990-03-25', '345678901', 'UeKat44!', 'UeKat4@gmail.com', 'Kobieta'),
+(5, 'Piotr', 'Kowalczyk', '78901234567', '1985-09-08', '789012345', 'UeKat55!', 'UeKat5@gmail.com', 'Mężczyzna'),
+(6, 'Alicja', 'Lis', '45678901234', '2000-07-30', '456789012', 'UeKat66!', 'UeKat6@gmail.com', 'Kobieta'),
+(7, 'Kamil', 'Kaczor', '23456789012', '1972-01-14', '234567890', 'UeKat77!', 'UeKat7@gmail.com', 'Mężczyzna'),
+(8, 'Karolina', 'Wójcik', '65432109876', '1988-11-05', '654321098', 'UeKat88!', 'UeKat8@gmail.com', 'Kobieta'),
+(9, 'Wojciech', 'Nowicki', '87654321098', '1993-04-18', '876543210', 'UeKat99!', 'UeKat9@gmail.com', 'Mężczyzna'),
+(10, 'Magdalena', 'Duda', '11223344556', '1974-06-22', '112233445', 'UeKat00!', 'UeKat10@gmail.com', 'Kobieta');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -65,16 +65,6 @@ INSERT INTO `pacjenci` (`IdPacjenta`, `Imie`, `Nazwisko`, `Pesel`, `DataUrodzeni
 ALTER TABLE `pacjenci`
   ADD PRIMARY KEY (`IdPacjenta`),
   ADD UNIQUE KEY `Unique_Pesel` (`Pesel`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `pacjenci`
---
-ALTER TABLE `pacjenci`
-  MODIFY `IdPacjenta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
