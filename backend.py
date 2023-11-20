fcabe85658ef7ed3690a61c0a6c984c515652f89
RZajacUE <remigiusz.zajac@edu.uekat.pl>
2023-11-20 20:39:31 +0100

    Form last changes and added colors

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from flask import Flask, render_template, request, redirect, url_for
 import re
+from colorama import Fore, Style, init as colorama_init
 
 mydb = mysql.connector.connect(
     host = "localhost", 
@@ -12,40 +13,52 @@ query = "SELECT * from pacjenci"
 query2 = "SELECT * from lekarze"
 query3 = "SELECT * from pokoje"
 query4 = "SELECT * from karty_pacjentow"
+query5 = "SELECT * from wizyty"
 
 cursor = mydb.cursor()
 cursor.execute(query)
 
-print('\nPacjenci')
+colorama_init()
+
+print(f'\n{Fore.YELLOW}Pacjenci')
 for x in cursor:
     for y in x:
         print(y, end=' ')
     print()
 
-# cursor.execute(query2)
+cursor.execute(query2)
 
-# print('\nLekarze')
-# for x in cursor:
-#     for y in x:
-#         print(y, end=' ')
-#     print()
+print('\nLekarze')
+for x in cursor:
+    for y in x:
+        print(y, end=' ')
+    print()
 
-# cursor.execute(query3)
+cursor.execute(query3)
 
-# print('\nPokoje')
-# for x in cursor:
-#     for y in x:
-#         print(y, end=' ')
-#     print()
-# print()
+print('\nPokoje')
+for x in cursor:
+    for y in x:
+        print(y, end=' ')
+    print()
+print()
 
-# cursor.execute(query4)
+cursor.execute(query4)
 
-# print('\nKarty_pacjentów')
-# for x in cursor:
-#     for y in x:
-#         print(y, end=' ')
-#     print()
+print('\nKarty_pacjentów')
+for x in cursor:
+    for y in x:
+        print(y, end=' ')
+    print()
+
+cursor.execute(query5)
+
+print('\nWizyty')
+for x in cursor:
+    for y in x:
+        print(y, end=' ')
+    print()
+print(f'{Style.RESET_ALL}')
 
 app = Flask(__name__)
 
@@ -58,45 +71,60 @@ def register():
         pesel = request.form.get("pesel")
         birthdate =  request.form.get("birthdate")
         telephone = request.form.get("telephone")
-        #email = request.form.get("email")
+        email = request.form.get("email")
         password = request.form.get("password")
         r_password = request.form.get("repeat_password")
         RODO = request.form.get("check")
         pattern = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$"
         pattern2 = "\d{11}"
-        pattern3 = ".+\@.+\..+"
-        pattern4 = "\d{9}"
+        pattern3 = ".+\@.+\..+|^0$"
+        pattern4 = "\d{9}|^0$"
 
-        print(f'{name} {surname} {sex} {pesel} {birthdate} {telephone} {password} {r_password} {RODO}') #mail
-        print(len(telephone))
-        if len(telephone) == 0: #mail
+        print(f'\n{Fore.BLUE}{name} {surname} {sex} {pesel} {birthdate} {telephone} {email} {password} {r_password} {RODO}{Style.RESET_ALL}\n')
+        if len(telephone) == 0:
             telephone = 0
+        if len(email) == 0:
+            email = 0
 
-        insert = f"INSERT into pacjenci (IdPacjenta, Imie, Nazwisko, Pesel, DataUrodzenia, NumerTel, Haslo) VALUES (null, '{name.capitalize()}', '{surname.capitalize()}', {pesel}, '{birthdate}', {telephone}, '{password}')"
-        print(insert)
+        insert = f"INSERT into pacjenci (IdPacjenta, Imie, Nazwisko, Pesel, DataUrodzenia, NumerTel, Haslo, Email, Plec) VALUES (null, '{name.capitalize()}', '{surname.capitalize()}', {pesel}, '{birthdate}', {telephone}, '{password}', '{email}', '{sex}')"
+        print(f'{Fore.GREEN}{insert}{Style.RESET_ALL}')
         pesels = f"SELECT * from pacjenci where Pesel = {pesel}"
+        if str(pesel):
+            pesels = f"SELECT * from pacjenci where Pesel = '{pesel}'"
         cursor.execute(pesels)
         
         password_strength = re.match(pattern, password)
         pesel_digits = re.match(pattern2, str(pesel))
-        #correct_email = re.match(pattern3, email)
+        correct_email = re.match(pattern3, str(email))
         phone_number_digits = re.match(pattern4, str(telephone))
 
         if pesel_digits == None:
             myresult4 = "Nieprawidłowy numer PESEL"
-            print("pesel digits")
+            print(f'{Fore.RED}{myresult4}{Style.RESET_ALL}\n')
             return render_template('rejestracja_konta.html', myresult4 = myresult4)
         elif cursor.fetchone():
             myresult5 = "Użytkownik o podanym numerze PESEL już istnieje!"
+            print(f'{Fore.RED}{myresult5}{Style.RESET_ALL}\n')
             return render_template('rejestracja_konta.html', myresult5 = myresult5)
+        elif phone_number_digits == None:
+            myresult6 = "Podano niepoprawny format numeru telefonu"
+            print(f'{Fore.RED}{myresult6}{Style.RESET_ALL}\n')
+            return render_template('rejestracja_konta.html', myresult6 = myresult6)
+        elif correct_email == None:
+            myresult7 = "Podany adres E-mail jest nieprawidłowy"
+            print(f'{Fore.RED}{myresult7}{Style.RESET_ALL}\n')
+            return render_template('rejestracja_konta.html', myresult7 = myresult7)
         elif password_strength == None:
             myresult3 = "Hasło nie spełnia wymagań. Poprawne hasło powinno mieć minimalnie 8 znaków w tym: 1 duża litera, 1 mała litera, 1 cyfra, 1 znak specjalny"
+            print(f'{Fore.RED}{myresult3}{Style.RESET_ALL}\n')
             return render_template('rejestracja_konta.html', myresult3 = myresult3)
         elif password != r_password:
             myresult = "Podane hasła różnią się!"
+            print(f'{Fore.RED}{myresult}{Style.RESET_ALL}\n')
             return render_template('rejestracja_konta.html', myresult = myresult)
         elif RODO != 'on':
             myresult2 = "Akceptacja RODO jest wymagana!"
+            print(f'{Fore.RED}{myresult2}{Style.RESET_ALL}\n')
             return render_template('rejestracja_konta.html', myresult2 = myresult2)
         else:
             cursor.execute(insert)
@@ -104,6 +132,9 @@ def register():
             if telephone == 0:
                 update = f"UPDATE pacjenci SET NumerTel = NULL WHERE IdPacjenta = (SELECT max(IdPacjenta) from pacjenci)"
                 cursor.execute(update)
+            if email == 0:
+                update1 = f"UPDATE pacjenci SET Email = NULL WHERE IdPacjenta = (SELECT max(IdPacjenta) from pacjenci)"
+                cursor.execute(update1)
 
             mydb.commit()
             return redirect(url_for('login'))
--- przychodnia.sql
+++ przychodnia.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Lis 19, 2023 at 05:59 PM
+-- Generation Time: Lis 20, 2023 at 05:08 PM
 -- Wersja serwera: 10.4.28-MariaDB
 -- Wersja PHP: 8.2.4
 
@@ -20,6 +20,48 @@ SET time_zone = "+00:00";
 --
 -- Database: `przychodnia`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `karty_pacjentow`
+--
+
+CREATE TABLE `karty_pacjentow` (
+  `IdKarty` int(11) NOT NULL,
+  `IdPacjenta` int(11) DEFAULT NULL,
+  `HistoriaChorob` text DEFAULT NULL,
+  `PrzebytyZabiegi` text DEFAULT NULL,
+  `PrzypisaneLeki` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `lekarze`
+--
+
+CREATE TABLE `lekarze` (
+  `IdLekarza` int(11) NOT NULL,
+  `Imie` varchar(255) DEFAULT NULL,
+  `Nazwisko` varchar(255) DEFAULT NULL,
+  `Specjalizacja` varchar(255) DEFAULT NULL,
+  `NrLicencjiLekarskiej` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Dumping data for table `lekarze`
+--
+
+INSERT INTO `lekarze` (`IdLekarza`, `Imie`, `Nazwisko`, `Specjalizacja`, `NrLicencjiLekarskiej`) VALUES
+(1, 'Adam', 'Nowak', 'Kardiolog', 'L12345'),
+(2, 'Ewa', 'Kowalska', 'Dermatolog', 'L54321'),
+(3, 'Marek', 'Wójcik', 'Ortopeda', 'L98765'),
+(4, 'Anna', 'Lis', 'Neurolog', 'L45678'),
+(5, 'Jan', 'Kaczor', 'Ginekolog', 'L87654'),
+(6, 'Karolina', 'Jankowska', 'Pediatra', 'L23456'),
+(7, 'Piotr', 'Wiśniewski', 'Chirurg', 'L65432'),
+(8, 'Alicja', 'Duda', 'Internista', 'L76543');
 
 -- --------------------------------------------------------
 
@@ -55,9 +97,133 @@ INSERT INTO `pacjenci` (`IdPacjenta`, `Imie`, `Nazwisko`, `Pesel`, `DataUrodzeni
 (9, 'Wojciech', 'Nowicki', '87654321098', '1993-04-18', '876543210', 'UeKat99!', 'UeKat9@gmail.com', 'Mężczyzna'),
 (10, 'Magdalena', 'Duda', '11223344556', '1974-06-22', '112233445', 'UeKat00!', 'UeKat10@gmail.com', 'Kobieta');
 
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `pokoje`
+--
+
+CREATE TABLE `pokoje` (
+  `IdPokoju` int(11) NOT NULL,
+  `NumerPokoju` int(11) DEFAULT NULL,
+  `Status` varchar(20) DEFAULT NULL,
+  `IdPrzychodni` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Dumping data for table `pokoje`
+--
+
+INSERT INTO `pokoje` (`IdPokoju`, `NumerPokoju`, `Status`, `IdPrzychodni`) VALUES
+(1, 101, 'Wolny', 1),
+(2, 102, 'Zajęty', 1),
+(3, 201, 'Wolny', 1),
+(4, 202, 'Zajęty', 1),
+(5, 301, 'Wolny', 1),
+(6, 302, 'Wolny', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `wizyty`
+--
+
+CREATE TABLE `wizyty` (
+  `IdWizyty` int(11) NOT NULL,
+  `DataWizyty` datetime DEFAULT NULL,
+  `IdPacjenta` int(11) DEFAULT NULL,
+  `IdLekarza` int(11) DEFAULT NULL,
+  `IdKarty` int(11) DEFAULT NULL,
+  `IdPokoju` int(11) DEFAULT NULL,
+  `IdPrzychodni` int(11) DEFAULT NULL,
+  `OpisWizyty` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
+
+--
+-- Dumping data for table `wizyty`
+--
+
+INSERT INTO `wizyty` (`IdWizyty`, `DataWizyty`, `IdPacjenta`, `IdLekarza`, `IdKarty`, `IdPokoju`, `IdPrzychodni`, `OpisWizyty`) VALUES
+(1, '2024-02-12 08:00:00', NULL, 1, NULL, NULL, NULL, NULL),
+(2, '2024-02-12 09:00:00', NULL, 1, NULL, NULL, NULL, NULL),
+(3, '2024-02-12 10:00:00', NULL, 1, NULL, NULL, NULL, NULL),
+(4, '2024-02-12 11:00:00', NULL, 1, NULL, NULL, NULL, NULL),
+(5, '2024-02-12 12:00:00', NULL, 1, NULL, NULL, NULL, NULL),
+(6, '2024-02-12 13:00:00', NULL, 1, NULL, NULL, NULL, NULL),
+(7, '2024-02-12 14:00:00', NULL, 1, NULL, NULL, NULL, NULL),
+(8, '2024-02-12 15:00:00', NULL, 1, NULL, NULL, NULL, NULL),
+(9, '2024-02-12 08:00:00', NULL, 2, NULL, NULL, NULL, NULL),
+(10, '2024-02-12 09:00:00', NULL, 2, NULL, NULL, NULL, NULL),
+(11, '2024-02-12 10:00:00', NULL, 2, NULL, NULL, NULL, NULL),
+(12, '2024-02-12 11:00:00', NULL, 2, NULL, NULL, NULL, NULL),
+(13, '2024-02-12 12:00:00', NULL, 2, NULL, NULL, NULL, NULL),
+(14, '2024-02-12 13:00:00', NULL, 2, NULL, NULL, NULL, NULL),
+(15, '2024-02-12 14:00:00', NULL, 2, NULL, NULL, NULL, NULL),
+(16, '2024-02-12 15:00:00', NULL, 2, NULL, NULL, NULL, NULL),
+(17, '2024-02-12 08:00:00', NULL, 3, NULL, NULL, NULL, NULL),
+(18, '2024-02-12 09:00:00', NULL, 3, NULL, NULL, NULL, NULL),
+(19, '2024-02-12 10:00:00', NULL, 3, NULL, NULL, NULL, NULL),
+(20, '2024-02-12 11:00:00', NULL, 3, NULL, NULL, NULL, NULL),
+(21, '2024-02-12 12:00:00', NULL, 3, NULL, NULL, NULL, NULL),
+(22, '2024-02-12 13:00:00', NULL, 3, NULL, NULL, NULL, NULL),
+(23, '2024-02-12 14:00:00', NULL, 3, NULL, NULL, NULL, NULL),
+(24, '2024-02-12 15:00:00', NULL, 3, NULL, NULL, NULL, NULL),
+(25, '2024-02-12 08:00:00', NULL, 4, NULL, NULL, NULL, NULL),
+(26, '2024-02-12 09:00:00', NULL, 4, NULL, NULL, NULL, NULL),
+(27, '2024-02-12 10:00:00', NULL, 4, NULL, NULL, NULL, NULL),
+(28, '2024-02-12 11:00:00', NULL, 4, NULL, NULL, NULL, NULL),
+(29, '2024-02-12 12:00:00', NULL, 4, NULL, NULL, NULL, NULL),
+(30, '2024-02-12 13:00:00', NULL, 4, NULL, NULL, NULL, NULL),
+(31, '2024-02-12 14:00:00', NULL, 4, NULL, NULL, NULL, NULL),
+(32, '2024-02-12 15:00:00', NULL, 4, NULL, NULL, NULL, NULL),
+(33, '2024-02-12 08:00:00', NULL, 5, NULL, NULL, NULL, NULL),
+(34, '2024-02-12 09:00:00', NULL, 5, NULL, NULL, NULL, NULL),
+(35, '2024-02-12 10:00:00', NULL, 5, NULL, NULL, NULL, NULL),
+(36, '2024-02-12 11:00:00', NULL, 5, NULL, NULL, NULL, NULL),
+(37, '2024-02-12 12:00:00', NULL, 5, NULL, NULL, NULL, NULL),
+(38, '2024-02-12 13:00:00', NULL, 5, NULL, NULL, NULL, NULL),
+(39, '2024-02-12 14:00:00', NULL, 5, NULL, NULL, NULL, NULL),
+(40, '2024-02-12 15:00:00', NULL, 5, NULL, NULL, NULL, NULL),
+(41, '2024-02-12 08:00:00', NULL, 6, NULL, NULL, NULL, NULL),
+(42, '2024-02-12 09:00:00', NULL, 6, NULL, NULL, NULL, NULL),
+(43, '2024-02-12 10:00:00', NULL, 6, NULL, NULL, NULL, NULL),
+(44, '2024-02-12 11:00:00', NULL, 6, NULL, NULL, NULL, NULL),
+(45, '2024-02-12 12:00:00', NULL, 6, NULL, NULL, NULL, NULL),
+(46, '2024-02-12 13:00:00', NULL, 6, NULL, NULL, NULL, NULL),
+(47, '2024-02-12 14:00:00', NULL, 6, NULL, NULL, NULL, NULL),
+(48, '2024-02-12 15:00:00', NULL, 6, NULL, NULL, NULL, NULL),
+(49, '2024-02-12 08:00:00', NULL, 7, NULL, NULL, NULL, NULL),
+(50, '2024-02-12 09:00:00', NULL, 7, NULL, NULL, NULL, NULL),
+(51, '2024-02-12 10:00:00', NULL, 7, NULL, NULL, NULL, NULL),
+(52, '2024-02-12 11:00:00', NULL, 7, NULL, NULL, NULL, NULL),
+(53, '2024-02-12 12:00:00', NULL, 7, NULL, NULL, NULL, NULL),
+(54, '2024-02-12 13:00:00', NULL, 7, NULL, NULL, NULL, NULL),
+(55, '2024-02-12 14:00:00', NULL, 7, NULL, NULL, NULL, NULL),
+(56, '2024-02-12 15:00:00', NULL, 7, NULL, NULL, NULL, NULL),
+(57, '2024-02-12 08:00:00', NULL, 8, NULL, NULL, NULL, NULL),
+(58, '2024-02-12 09:00:00', NULL, 8, NULL, NULL, NULL, NULL),
+(59, '2024-02-12 10:00:00', NULL, 8, NULL, NULL, NULL, NULL),
+(60, '2024-02-12 11:00:00', NULL, 8, NULL, NULL, NULL, NULL),
+(61, '2024-02-12 12:00:00', NULL, 8, NULL, NULL, NULL, NULL),
+(62, '2024-02-12 13:00:00', NULL, 8, NULL, NULL, NULL, NULL),
+(63, '2024-02-12 14:00:00', NULL, 8, NULL, NULL, NULL, NULL),
+(64, '2024-02-12 15:00:00', NULL, 8, NULL, NULL, NULL, NULL);
+
 --
 -- Indeksy dla zrzutów tabel
 --
+
+--
+-- Indeksy dla tabeli `karty_pacjentow`
+--
+ALTER TABLE `karty_pacjentow`
+  ADD PRIMARY KEY (`IdKarty`);
+
+--
+-- Indeksy dla tabeli `lekarze`
+--
+ALTER TABLE `lekarze`
+  ADD PRIMARY KEY (`IdLekarza`);
 
 --
 -- Indeksy dla tabeli `pacjenci`
@@ -65,6 +231,52 @@ INSERT INTO `pacjenci` (`IdPacjenta`, `Imie`, `Nazwisko`, `Pesel`, `DataUrodzeni
 ALTER TABLE `pacjenci`
   ADD PRIMARY KEY (`IdPacjenta`),
   ADD UNIQUE KEY `Unique_Pesel` (`Pesel`);
+
+--
+-- Indeksy dla tabeli `pokoje`
+--
+ALTER TABLE `pokoje`
+  ADD PRIMARY KEY (`IdPokoju`);
+
+--
+-- Indeksy dla tabeli `wizyty`
+--
+ALTER TABLE `wizyty`
+  ADD PRIMARY KEY (`IdWizyty`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `karty_pacjentow`
+--
+ALTER TABLE `karty_pacjentow`
+  MODIFY `IdKarty` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `lekarze`
+--
+ALTER TABLE `lekarze`
+  MODIFY `IdLekarza` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `pacjenci`
+--
+ALTER TABLE `pacjenci`
+  MODIFY `IdPacjenta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `pokoje`
+--
+ALTER TABLE `pokoje`
+  MODIFY `IdPokoju` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `wizyty`
+--
+ALTER TABLE `wizyty`
+  MODIFY `IdWizyty` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
